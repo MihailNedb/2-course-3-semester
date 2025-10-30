acdcfc54fb7ef2b49937900426d188491b1262ba
Михаил Недбальский <nedbalski.1kyrc.6grupa@gmail.com>
2025-10-30 09:54:32 +0300

    Add files via upload

---
+++ АиСД/BinKucha_0.2_AaDS.cpp
@@ -0,0 +1,62 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+void rec(int* m,int size1,bool& check1,int& i)
+{
+	if (check1 == true)
+	{
+		if (2 * i + 1 <= size1-1)
+		{
+			if (m[i] > m[2 * i + 1])
+			{
+				check1 = false;
+				return;
+			}
+			int newi = 2 * i + 1;
+			rec(m, size1, check1, newi);
+		}
+		if (2 * i + 2 <= size1-1)
+		{
+			if (m[i] > m[2 * i + 2])
+			{
+				check1 = false;
+				return;
+			}
+			int newi = 2 * i + 2;
+			rec(m, size1, check1, newi);
+		}
+	}
+	return;
+}
+int main()
+{
+	ifstream in("input.txt");
+	ofstream out("output.txt");
+
+	int size1 = 0;
+	in >> size1;
+	int* m = new int[size1];
+	for (int i = 0; i < size1; i++)
+	{
+		in >> m[i];
+	}
+	int i = 0;
+	bool check1 = true;
+	rec(m, size1, check1, i);
+	cout << check1;
+	if (check1 == true)
+	{
+		out << "Yes";
+	}
+	else
+	{
+		out << "No";
+	}
+
+	in.close();
+	out.close();
+}
+
