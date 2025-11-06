520e673d43a2d5ba81a1d6c3cc654d57cf753420
Михаил Недбальский <nedbalski.1kyrc.6grupa@gmail.com>
2025-11-06 09:47:26 +0300

    Add files via upload

---
+++ АиСД/AaDS_0.5_roads.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+long long find(long long* m, long long city)
+{
+    long long root = city;
+    // Находим корень
+    while (m[root] != root)
+    {
+        root = m[root];
+    }
+
+    // Сжатие пути
+    long long temp = city;
+    while (m[temp] != temp)
+    {
+        long long next = m[temp];
+        m[temp] = root;
+        temp = next;
+    }
+
+    return root;
+}
+
+bool union1(long long* m, long long city1, long long city2, long long kolgorodov)
+{
+    long long c1 = find(m, city1);
+    long long c2 = find(m, city2);
+
+    if (c1 == c2)
+    {
+        return false;
+    }
+
+    m[c2] = c1;
+    return true;
+}
+
+int main()
+{
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    long long kolgorodov = 0;
+    in >> kolgorodov;
+    long long koldorog;
+    in >> koldorog;
+
+    long long* m = new long long[kolgorodov];
+    for (long long i = 0; i < kolgorodov; i++)
+    {
+        m[i] = i;
+    }
+
+    long long city1, city2;
+    bool check = false;
+    long long comp = kolgorodov;
+
+    for (long long i = 0; i < koldorog; i++)
+    {
+        in >> city1;
+        in >> city2;
+        city1--;
+        city2--;
+
+        check = union1(m, city1, city2, kolgorodov);
+        if (check == true)
+        {
+            comp--;
+        }
+        out << comp;
+        out << '\n';
+    }
+
+    delete[] m;
+    in.close();
+    out.close();
+    return 0;
+}
---
+++ АиСД/AaDS_Haffman_0.4.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <fstream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ifstream in("huffman.in");
+    ofstream out("huffman.out");
+
+    int size1 = 0;
+    in >> size1;
+    queue<long long> m;
+
+    for (int i = 0; i < size1; i++)
+    {
+        long long a;
+        in >> a;
+        m.push(a);
+    }
+
+    long long mysum = 0;
+    long long sum = 0;
+    queue<long long> m2;
+    while (m.size() > 0 || m2.size() > 0)
+    {
+        long long ch1, ch2;
+        if (m2.size() == 0 && m.size() == 0)
+        {
+            break;
+        }
+        if (m2.size() == 0)
+        {
+            ch1 = m.front();
+            m.pop();
+        }
+        else if (m.size() == 0)
+        {
+            ch1 = m2.front();
+            m2.pop();
+        }
+        else if (m2.front() > m.front())
+        {
+            ch1 = m.front();
+            m.pop();
+        }
+        else
+        {
+            ch1 = m2.front();
+            m2.pop();
+        }
+
+
+        if (m2.size() == 0 && m.size() == 0)
+        {
+            break;
+        }
+        if (m2.size() == 0)
+        {
+            ch2 = m.front();
+            m.pop();
+        }
+        else if (m.size() == 0)
+        {
+            ch2 = m2.front();
+            m2.pop();
+        }
+        else if (m2.front() > m.front())
+        {
+            ch2 = m.front();
+            m.pop();
+        }
+        else
+        {
+            ch2 = m2.front();
+            m2.pop();
+        }
+
+        long long sum = ch1 + ch2;
+        mysum = mysum + sum;
+        m2.push(sum);
+    }
+
+    out << mysum;
+
+    return 0;
+}
