27a21201af67f24759e1e1002e5ef11cac00331f
MihailNedb <nedbalski.1kyrc.6grupa@gmail.com>
2026-01-01 18:49:48 +0300

    removed unnecessary files

--- ОС/OsLab1/creator/employee.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct employee
-{
-    int num;        // идентификационный номер сотрудника
-    char name[10];  // имя сотрудника
-    double hours;   // количество отработанных часов
-};
